7c3e56b816629f9ed0e9a690564f1e4fed4a1627
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-03-17 22:24:46 -0600

    boilerplaite fixed

---
+++ js/main.js
@@ -0,0 +1,3 @@
+document.getElementById('animal-sound').innerHTML = 'shsh';
+document.getElementById('dog-sound').innerHTML = 'wof wof';
+document.getElementById('bird-sound').innerHTML = 'cri cri';
