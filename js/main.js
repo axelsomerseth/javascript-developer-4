9f21714a6bb32a7973747137432ef7e436e27953
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-03-18 10:10:05 -0600

    hanlding prototypes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-document.getElementById('animal-sound').innerHTML = 'shsh';
-document.getElementById('dog-sound').innerHTML = 'wof wof';
-document.getElementById('bird-sound').innerHTML = 'cri cri';
+import { Animal, Dog, Bird } from './zoo.js';
+
+const cricket = new Animal('cricket', 'cri cri');
+console.log(Animal.prototype);
+
+
+const firulais = new Dog('firulais', 'WOOF WOOF!!');
+console.log(Dog.prototype);
+console.log(Animal.prototype.hasOwnProperty('makeSound'));
+console.log(Dog.prototype.hasOwnProperty('bark'));
+
+const parrot = new Bird('parrot', 'twit twit');
+console.log(Bird.prototype);
+
+
+const animalSound = document.getElementById('animal-sound');
+const dogSound = document.getElementById('dog-sound');
+const birdSound = document.getElementById('bird-sound');
+
+animalSound.innerHTML = cricket.makeSound();
+dogSound.innerHTML = firulais.makeSound();
+birdSound.innerHTML = parrot.makeSound();
---
+++ js/zoo.js
@@ -0,0 +1,26 @@
+class Animal {
+	constructor(name, sound) {
+		this.name = name;
+		this.sound = sound;
+	}
+	makeSound() {
+		return this.sound;
+	}
+}
+
+class Dog extends Animal {
+	constructor(name, sound) {
+		super(name, sound);
+	}
+	bark() {
+		return this.sound;
+	}
+}
+
+class Bird extends Animal {
+	constructor(name, sound) {
+		super(name, sound);
+	}
+}
+
+export { Animal, Dog, Bird };
