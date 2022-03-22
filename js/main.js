e7d6845fa4071bdc8bb28e72f7714f55165b077b
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-03-22 14:22:16 -0600

    imports done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 document.getElementById('animal-sound').innerHTML = 'shsh';
 document.getElementById('dog-sound').innerHTML = 'wof wof';
 document.getElementById('bird-sound').innerHTML = 'cri cri';
+
+// 1. Import
+console.log(_.upperCase('hola axel'));
